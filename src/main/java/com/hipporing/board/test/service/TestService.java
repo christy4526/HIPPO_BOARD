63f4cb6e613debcfea7128492851a4ff52e31869
christy4526 <christy4526@gmail.com>
2020-07-18 17:08:44 +0900

    first commit

---
+++ src/main/java/com/hipporing/board/test/service/TestService.java
@@ -0,0 +1,28 @@
+package com.hipporing.board.test.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hipporing.board.test.dao.TestDao;
+import com.hipporing.board.test.vo.TestVO;
+
+@Service
+public class TestService {
+	
+	@Autowired
+	private TestDao testDao;
+	
+	// TestVO의 값을 받아오는 함수를 TestDao에서 호출하도록 하는 함수
+	// private TestDao testDao = new TestDao();
+	// component를 bean이라고 함
+	public TestVO getTest(int key) {
+		return this.testDao.getTest(key);
+	}
+	
+	public List<TestVO> getTests() {
+		return this.testDao.getTests();
+	}
+
+}
