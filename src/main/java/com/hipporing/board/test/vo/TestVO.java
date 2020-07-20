c022b1f9c8202ddc38ed2c6ec01c5fa4a571d74c
christy4526 <christy4526@gmail.com>
2020-07-20 22:05:08 +0900

    홋터디 2차시 Insert, Delete, lombok

--- src/main/java/com/hipporing/board/test/vo/TestVO.java
+++ src/main/java/com/hipporing/board/test/vo/TestVO.java
@@ -1,30 +1,16 @@
 package com.hipporing.board.test.vo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 //data를 받는 틀
+@Getter
+@Setter
+@ToString
 public class TestVO {
 	private String param1;
 	private String param2;
 	private String param3;
-	
-	public String getParam1() {
-		return param1;
-	}
-	public void setParam1(String param1) {
-		this.param1 = param1;
-	}
-	public String getParam2() {
-		return param2;
-	}
-	public void setParam2(String param2) {
-		this.param2 = param2;
-	}
-	public String getParam3() {
-		return param3;
-	}
-	public void setParam3(String param3) {
-		this.param3 = param3;
-	}	
+	private int key;
 }
--- src/test/java/com/hipporing/board/BoardApplicationTests.java
+++ src/test/java/com/hipporing/board/BoardApplicationTests.java
@@ -1,13 +1,25 @@
 package com.hipporing.board;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.hipporing.board.test.service.TestService;
+import com.hipporing.board.test.vo.TestVO;
+
+
+//Test 전용 유닛
+@SpringBootTest //Test를 위해서 서버를 켜겠다
 class BoardApplicationTests {
+	@Autowired
+	private TestService testService;
 
 	@Test
 	void contextLoads() {
+		int key = 1;
+		TestVO test = this.testService.getTest(key);
+
+		System.out.println(test.toString());
 	}
 
 }
